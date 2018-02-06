a06af8295e8a9ddb9ad252b72e5a470493f3d280
Dominik Króliczek <kroliczeek1@gmail.com>
2018-02-06 14:13:08 +0100

    Write test to deposit contract

--- test/deposit.test.ts
+++ test/deposit.test.ts
@@ -11,12 +11,45 @@ const DepositContract = artifacts.require('./Deposit.sol');
 
 contract('Deposit', accounts => {
     const owner = accounts[0];
+
     let deposit: Deposit;
+
+    beforeEach(async () => {
+        deposit = await DepositContract.new({from: owner});
+    });
 
     describe('#constructor', () => {
         it('should create contract', async () => {
-            deposit = await DepositContract.new({from: owner});
             assert.isOk(deposit);
+        });
+    });
+
+    describe('#deposit', () => {
+        context('When is in db', () => {
+            it('Should be able to deposit', async () => {
+                assert.fail();
+            });
+        });
+
+
+        context('When is not in db', () => {
+            it('Should not be able to deposit', async () => {
+                assert.fail();
+            });
+        });
+    });
+
+    describe('#withdraw', () => {
+        context('When is not in db', () => {
+            it('Should be able to withdraw', async () => {
+                assert.fail();
+            });
+        });
+
+        context('When is not in db', () => {
+            it('Should not be able to withdraw', async () => {
+                assert.fail();
+            });
         });
     });
 });
